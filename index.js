34c2bc7b048628b8f15618f70d23cc4f92a31073
greatdane <vanderbyljeandre@gmail.com>
2023-09-21 18:17:52 +0200

    update code

--- index.js
+++ index.js
@@ -5,5 +5,21 @@
 const imgs = [
     "images/hip1.jpg",
     "images/hip2.jpg",
-    "images/hip3.jpg"
+    "images/hip3.jpg",
+    "images/AMD.jpg",
+    "images/Intel.jpeg",
+    "images/Ryzen.png"
 ]
+
+const container = document.getElementById("container")
+
+function renderImages(){
+    let imgsDom = ""
+    for (let i = 0; i < imgs.length; i++){
+        //container.innerHTML += `<img class = "team-img" src="${imgs[i]}">` for performance reasons its better to 
+        imgsDom +=`<img alt="employee in the company" class = "team-img" src="${imgs[i]}">`
+    }
+    container.innerHTML =imgsDom
+
+}
+renderImages()
